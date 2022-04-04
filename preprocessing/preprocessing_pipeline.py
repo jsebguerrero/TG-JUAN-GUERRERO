c70c3d1b98ab5179fddac5f36f7556ec89aa280a
Juan S Guerrero C <36944059+jsebguerrero@users.noreply.github.com>
2022-04-03 21:40:32 -0500

    Add files via upload

--- preprocessing/preprocessing_pipeline.py
+++ preprocessing/preprocessing_pipeline.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 from preprocessing import preprocessing_functions
-
+from sklearn.preprocessing import MinMaxScaler
+import skimage
 
 def pipeline_csv(input_dir, output_dir, columns, skiprows=0, sep=',', fs=50):
     # Check whether the processed dir exists or not
@@ -21,5 +22,22 @@ def pipeline_csv(input_dir, output_dir, columns, skiprows=0, sep=',', fs=50):
         df = preprocessing_functions.resample(data_frame=df, fs=fs, columns=columns)
         # Filter for gravity
         df = preprocessing_functions.filter_gravity(data_frame=df)
+        # Scale down everything to -1 to 1, and assign class label
+        scaler = MinMaxScaler((-1, 1))
+        df[df.columns] = scaler.fit_transform(df[df.columns])
         # Save again
         df.to_csv(os.path.join(output_dir, file))
+
+
+def pipeline_split_data(directory, window_size, overlap):
+    X = [[], [], []]
+    y = [[], [], []]
+    for file in os.listdir(directory):
+        df = pd.read_csv(os.path.join(directory, file))
+        X[0].append(skimage.util.view_as_windows(df.iloc[:, 0].values, window_size, step=overlap))
+        y[0].append([1 if 'PD' in file else 0])
+        X[1].append(skimage.util.view_as_windows(df.iloc[:, 1].values, window_size, step=overlap))
+        y[1].append([1 if 'PD' in file else 0])
+        X[2].append(skimage.util.view_as_windows(df.iloc[:, 2].values, window_size, step=overlap))
+        y[2].append([1 if 'PD' in file else 0])
+    return X, y
